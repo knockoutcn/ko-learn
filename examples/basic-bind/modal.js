a127060bdedf99ded809ccecb8ffec210eb1ea34
wikies.wan <wikies.wan@vipshop.com>
2015-04-29 17:00:07 +0800

    bs

--- examples/basic-bind/modal.js
+++ examples/basic-bind/modal.js
@@ -22,6 +22,30 @@ $(function(){
 	    viewModel.details("<p>knockout中文站的地址是 <a href='http://knockoutcn.github.io' target='blank'>here</a>.</p>"); 
 	    ko.applyBindings(viewModel,$('#id-html')[0]);
 	})();
+
+	(function(){
+		var viewModel = {
+	        currentProfit: ko.observable(150000) // Positive value, so initially we don't apply the "profitWarning" class
+	    };
+	    viewModel.currentProfit(-50); 
+	    ko.applyBindings(viewModel,$('#id-css')[0]);
+	})();
+
+	(function(){
+		var viewModel = {
+	        currentProfit: ko.observable(150000) // Positive value, so initially we don't apply the "profitWarning" class
+	    };
+	    viewModel.currentProfit(-50); 
+	    ko.applyBindings(viewModel,$('#id-style')[0]);
+	})();
 	
+	(function(){
+		var viewModel = {
+	        url: ko.observable("http://knockoutcn.github.io"),
+	        details: ko.observable("knockout 中文站"),
+	        target :  ko.observable("blank")
+	    };
+	    ko.applyBindings(viewModel,$('#id-attr')[0]);
+	})();
 
 })
